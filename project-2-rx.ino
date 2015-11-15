412d3d9f2cfd5690e3874a2d6094387420ada7cd
David Siver <dsiver1@my.westga.edu>
2015-11-15 18:52:33 -0500

    added relay pin and control constants, closing and opening relay in setup to test it out

--- project-2-rx.ino
+++ project-2-rx.ino
@@ -1,15 +1,26 @@
 /*
  * Project 2 Receiver
  * Board: Arduino UNO or similar with ATmega328P
+ * Relay: Parallax, Inc. Single Relay Board (#27115)
  *
  * David Siver
  * CS 4985
  * Fall 2015
  */
 
-void setup() {
-  // put your setup code here, to run once:
+#define RELAY_PIN 7
+#define RELAY_NO_CLOSED HIGH
+#define RELAY_NO_OPEN LOW
 
+void setup() {
+  Serial.begin(9600);
+  pinMode(RELAY_PIN, OUTPUT);
+  delay(5000);
+  digitalWrite(RELAY_PIN, RELAY_NO_CLOSED);
+  Serial.println("RELAY_NO_CLOSED");
+  delay(1000);
+  digitalWrite(RELAY_PIN, RELAY_NO_OPEN);
+  Serial.println("RELAY_NO_OPEN");
 }
 
 void loop() {
