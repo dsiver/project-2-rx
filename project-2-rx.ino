f8ba727d910a62779d5d6a8d8bbb52eb56a67cbe
David Siver <dsiver1@my.westga.edu>
2015-11-15 20:26:01 -0500

    added method to toggle LED, refactored out relay test to toggle method

--- project-2-rx.ino
+++ project-2-rx.ino
@@ -11,19 +11,47 @@
 #define RELAY_PIN 7
 #define RELAY_NO_CLOSED HIGH
 #define RELAY_NO_OPEN LOW
+#define LED_PIN 8
+#define OFF "off"
+#define ON "on"
 
 void setup() {
   Serial.begin(9600);
   pinMode(RELAY_PIN, OUTPUT);
-  delay(5000);
-  digitalWrite(RELAY_PIN, RELAY_NO_CLOSED);
-  Serial.println("RELAY_NO_CLOSED");
-  delay(1000);
-  digitalWrite(RELAY_PIN, RELAY_NO_OPEN);
-  Serial.println("RELAY_NO_OPEN");
+  pinMode(LED_PIN, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  test();
+}
 
+void test(){
+  toggleRelay(ON);
+  toggleLED(ON);
+  delay(5000);
+  toggleRelay(OFF);
+  toggleLED(OFF);
+  delay(5000);
+}
+
+void toggleLED(String mode){
+  if(mode == ON){
+    digitalWrite(LED_PIN, HIGH);
+    Serial.println("LED ON");
+  }
+  else if(mode == OFF){
+    digitalWrite(LED_PIN, LOW);
+    Serial.println("LED OFF");
+  }
+}
+
+void toggleRelay(String mode){
+  if(mode == ON){
+    digitalWrite(RELAY_PIN, RELAY_NO_CLOSED);
+    Serial.println("RELAY_NO_CLOSED");
+  }
+  else if(mode == OFF){
+    digitalWrite(RELAY_PIN, RELAY_NO_OPEN);
+    Serial.println("RELAY_NO_OPEN");
+  }
 }
